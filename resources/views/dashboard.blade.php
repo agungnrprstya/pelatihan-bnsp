a503f4cc3266a8fdb8329d7b6d2543e7d0f537ab
agungnrprstya <agungnurprasetya@gmail.com>
2023-08-26 20:41:50 +0700

    fix: 🐛 delete cart

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,161 +25,161 @@
 <body id="page-top">
     @extends('index')
     @section('content')
-    <!-- Page Wrapper -->
-    <div id="wrapper">
+        <!-- Page Wrapper -->
+        <div id="wrapper">
 
-        <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+            <!-- Sidebar -->
+            <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/dashboard') }}">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-            </a>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
-
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="{{ url('/dashboard') }}">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Interface
-            </div>
-
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+                <!-- Sidebar - Brand -->
+                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/dashboard') }}">
+                    <div class="sidebar-brand-icon rotate-n-15">
+                        <i class="fas fa-laugh-wink"></i>
+                    </div>
+                    <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
                 </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
-                    </div>
+
+                <!-- Divider -->
+                <hr class="sidebar-divider my-0">
+
+                <!-- Nav Item - Dashboard -->
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ url('/dashboard') }}">
+                        <i class="fas fa-fw fa-tachometer-alt"></i>
+                        <span>Dashboard</span></a>
+                </li>
+
+                <!-- Divider -->
+                <hr class="sidebar-divider">
+
+                <!-- Heading -->
+                <div class="sidebar-heading">
+                    Interface
                 </div>
-            </li>
 
-            <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
-                </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
+                <!-- Nav Item - Pages Collapse Menu -->
+                <li class="nav-item">
+                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                        aria-expanded="true" aria-controls="collapseTwo">
+                        <i class="fas fa-fw fa-cog"></i>
+                        <span>Components</span>
+                    </a>
+                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                        <div class="bg-white py-2 collapse-inner rounded">
+                            <h6 class="collapse-header">Custom Components:</h6>
+                            <a class="collapse-item" href="buttons.html">Buttons</a>
+                            <a class="collapse-item" href="cards.html">Cards</a>
+                        </div>
                     </div>
+                </li>
+
+                <!-- Nav Item - Utilities Collapse Menu -->
+                <li class="nav-item">
+                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                        aria-expanded="true" aria-controls="collapseUtilities">
+                        <i class="fas fa-fw fa-wrench"></i>
+                        <span>Utilities</span>
+                    </a>
+                    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                        data-parent="#accordionSidebar">
+                        <div class="bg-white py-2 collapse-inner rounded">
+                            <h6 class="collapse-header">Custom Utilities:</h6>
+                            <a class="collapse-item" href="utilities-color.html">Colors</a>
+                            <a class="collapse-item" href="utilities-border.html">Borders</a>
+                            <a class="collapse-item" href="utilities-animation.html">Animations</a>
+                            <a class="collapse-item" href="utilities-other.html">Other</a>
+                        </div>
+                    </div>
+                </li>
+
+                <!-- Divider -->
+                <hr class="sidebar-divider">
+
+                <!-- Heading -->
+                <div class="sidebar-heading">
+                    Addons
                 </div>
-            </li>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+                <!-- Nav Item - Charts -->
+                <li class="nav-item">
+                    <a class="nav-link" href="charts.html">
+                        <i class="fas fa-fw fa-chart-area"></i>
+                        <span>Charts</span></a>
+                </li>
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Addons
-            </div>
-            
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
-            </li>
-
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
-            </li>
-            <hr class="sidebar-divider d-none d-md-block">
-        </ul>
-        <div id="content-wrapper" class="d-flex flex-column">
-            <div id="content" class="flex">
-                <div class="container-fluid">
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        {{-- <h1 class="h3 mb-0 text-gray-800">Dashboard</h1> --}}
-                    </div>
-                    <div class="justify-center">
-                        <div class="flex flex-col justify-center">
-                            <a href="{{ url('/add-cart') }}"><button
-                                    class="bg-blue-500 rounded-lg text-white p-2 mb-5">Tambah Cart</button></a>
-                            <table class="table-fixed w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
-                                <thead
-                                    class="text-xs text-white uppercase bg-gray-50 dark:bg-blue-500 dark:text-gray-400 rounded-full">
-                                    <tr>
-                                        <th scope="col" class="py-3 pl-3 rounded-ss-xl">
-                                            Product name
-                                        </th>
-                                        <th scope="col" class="py-3">
-                                            Jumlah
-                                        </th>
-                                        <th scope="col" class="py-3">
-                                            Harga Total
-                                        </th>
-                                        <th scope="col" class="py-3 rounded-se-xl">
-                                            Action
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @if ($data)
-                                    @foreach ($data as $item)
-                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                        <td class="py-3 pl-3 font-semibold text-black">
-                                            {{ $item->nama_produk }}
-                                        </td>
-                                        <td class="py-3 font-semibold text-black">
-                                            {{ $item->jumlah }}
-                                        </td>
-                                        <td class="py-3 font-semibold text-black">
-                                            Rp.{{ number_format($item->harga_total, 0) }}
-                                        </td>
-                                        <td class=" flex gap-4 py-3">
-                                            <a href="{{ url('edit-cart/' . $item->id) }}"><button
-                                                    class="bg-blue-500 text-white rounded-lg p-2 hover:brightness-125 transition-all">Update</button></a>
-                                            <a href="{{ url('delete-product/' . $item->id) }}"><button
-                                                    class="bg-red-500 text-white rounded-lg p-2 hover:brightness-125 transition-all">Delete</button></a>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                    @else
-                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                        <td colspan="100%"
-                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            Data belum tersedia
-                                        </td>
-                                    </tr>
-                                    @endif
-                                </tbody>
-                            </table>
+                <!-- Nav Item - Tables -->
+                <li class="nav-item">
+                    <a class="nav-link" href="tables.html">
+                        <i class="fas fa-fw fa-table"></i>
+                        <span>Tables</span></a>
+                </li>
+                <hr class="sidebar-divider d-none d-md-block">
+            </ul>
+            <div id="content-wrapper" class="d-flex flex-column">
+                <div id="content" class="flex">
+                    <div class="container-fluid">
+                        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                            {{-- <h1 class="h3 mb-0 text-gray-800">Dashboard</h1> --}}
+                        </div>
+                        <div class="justify-center">
+                            <div class="flex flex-col justify-center">
+                                <a href="{{ url('/add-cart') }}"><button
+                                        class="bg-blue-500 rounded-lg text-white p-2 mb-5">Tambah Cart</button></a>
+                                <table class="table-fixed w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+                                    <thead
+                                        class="text-xs text-white uppercase bg-gray-50 dark:bg-blue-500 dark:text-gray-400 rounded-full">
+                                        <tr>
+                                            <th scope="col" class="py-3 pl-3 rounded-ss-xl">
+                                                Product name
+                                            </th>
+                                            <th scope="col" class="py-3">
+                                                Jumlah
+                                            </th>
+                                            <th scope="col" class="py-3">
+                                                Harga Total
+                                            </th>
+                                            <th scope="col" class="py-3 rounded-se-xl">
+                                                Action
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @if ($data)
+                                            @foreach ($data as $item)
+                                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                                    <td class="py-3 pl-3 font-semibold text-black">
+                                                        {{ $item->nama_produk }}
+                                                    </td>
+                                                    <td class="py-3 font-semibold text-black">
+                                                        {{ $item->jumlah }}
+                                                    </td>
+                                                    <td class="py-3 font-semibold text-black">
+                                                        Rp.{{ number_format($item->harga_total, 0) }}
+                                                    </td>
+                                                    <td class=" flex gap-4 py-3">
+                                                        <a href="{{ url('edit-cart/' . $item->id) }}"><button
+                                                                class="bg-blue-500 text-white rounded-lg p-2 hover:brightness-125 transition-all">Update</button></a>
+                                                        <a href="{{ url('delete-cart/' . $item->id) }}"><button
+                                                                class="bg-red-500 text-white rounded-lg p-2 hover:brightness-125 transition-all">Delete</button></a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        @else
+                                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                                <td colspan="100%"
+                                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                    Data belum tersedia
+                                                </td>
+                                            </tr>
+                                        @endif
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     @endsection
     <!-- Bootstrap core JavaScript-->
     <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
